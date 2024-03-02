80f7c64e308e97e1fd60177302dacaa399f87a3b
Nibelheims <niflheims@gmail.com>
2024-03-02 19:25:56 +0100

    nicely animated bars

--- keyboards/lily58/keymaps/nibelheims/keymap.c
+++ keyboards/lily58/keymaps/nibelheims/keymap.c
@@ -1,7 +1,19 @@
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 #include <stdint.h>
+
+#define HIDRAW_NAME_LEN     4
+#define HIDRAW_VALUE_LEN    1
+#define HIDRAW_DATA_LEN     (HIDRAW_NAME_LEN + HIDRAW_VALUE_LEN)
+#define MAX_NB_MEASURES     4
+
+#ifdef OLED_ENABLE
+// the bar is like: xxxx-[==========]xxx%
+#define BAR_NB_PX   60 // the screen is 128px wide, each glyph is 6 wide, use 6 before the bar and 5 after, 10 remaining
+#endif
 
 enum layer_number {
   _QWERTY = 0,
@@ -15,19 +27,36 @@ enum custom_keycodes {
 };
 
 typedef struct {
-  char name[4+1/*final null byte*/];
+  char name[HIDRAW_NAME_LEN+1/*final null byte*/];
   uint8_t percent;
 } measure_t;
 
+#ifdef OLED_ENABLE
+// bar animation stuff, so that transitions between values are smooth
+const uint16_t ANIM_DURATION = 300; //ms for a full bar travel
 typedef struct {
-  measure_t  measures[4]; // maximum nb of measures
-  size_t     nb_measures;
+    size_t   current_step;
+    size_t   nb_steps;
+    uint16_t ms_period; // per pixel
+    uint16_t timer;
+    uint8_t  px;
+    int8_t   increase; // direction, +1 or -1
+} animation_t;
+#endif
+
+typedef struct {
+  measure_t   measures[MAX_NB_MEASURES];
+  size_t      nb_measures;
+#ifdef OLED_ENABLE
+  animation_t animations[MAX_NB_MEASURES];
+#endif
 } measures_t;
 
 static bool prev_was_measures = false;
 static volatile measures_t measures;
 static volatile bool metrics_received = false;
-static deferred_token metrics_timeout_token = INVALID_DEFERRED_TOKEN;
+// make me volatile to use the horrible hack in metrics_timeout()
+static /*volatile*/ deferred_token metrics_timeout_token = INVALID_DEFERRED_TOKEN;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -129,11 +158,77 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
+void keyboard_pre_init_user(void) {
+  memset((void*)&measures, 0, sizeof(measures_t));
+  prev_was_measures = false;
+  metrics_received = false;
+  metrics_timeout_token = INVALID_DEFERRED_TOKEN;
+}
+
 //SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
 #ifdef OLED_ENABLE
 
+static inline int PERCENT_TO_PX(int p) {return (p * BAR_NB_PX)/100;}
+static inline int PX_TO_PERCENT(int p) {return (p * 100)/BAR_NB_PX;}
+
+static inline void init_animation(size_t anim_idx, uint8_t current_percent_value, uint8_t new_percent_value) {
+    volatile animation_t *p_animation = &measures.animations[anim_idx];
+
+     // if an animation is still on-going, keep the currently drawn value
+    if (p_animation->nb_steps == 0) {
+        // no on-going animation
+        p_animation->px = (uint8_t)PERCENT_TO_PX((int)current_percent_value);
+    } else {
+        // on-going animation, use the current bar level to compute the number of steps
+        current_percent_value = (uint8_t)PX_TO_PERCENT((int)(p_animation->px));
+        // keep p_animation->px
+    }
+    p_animation->increase = (new_percent_value > current_percent_value) ? 1 : -1;
+    p_animation->nb_steps = (size_t)abs(PERCENT_TO_PX((int)current_percent_value) - PERCENT_TO_PX((int)new_percent_value));
+    if (p_animation->nb_steps > 0) {
+        p_animation->ms_period = ANIM_DURATION / p_animation->nb_steps; // > 1
+    }
+    p_animation->current_step = 0;
+    p_animation->timer = timer_read();
+
+    return;
+}
+
+static inline void draw_bar(char *name, char *percent, uint8_t px) {
+    const int GLYPH_WIDTH = 6;
+    const int GLYPH_BAR_OFFSET = 3;
+    char bar[4+ 2+10+1 + 4 + 1]; // starting decoration, bar itself, ending decoration, final null byte
+
+    int nb_full   = px / GLYPH_WIDTH;
+    int remaining = px % GLYPH_WIDTH;
+    int i = 0;
+
+    // bar label
+    bar[0] = '\0';
+    strncat(bar, name, 4);
+
+    // bar init decoration
+    memcpy(&bar[4], "\x01\x02", 2);
+
+    // 10-glyph bar
+    for (i = 0; i < nb_full; i++) {
+        bar[6+i] = GLYPH_BAR_OFFSET + GLYPH_WIDTH;
+    }
+    bar[6+i] = GLYPH_BAR_OFFSET + remaining;
+    for (i++; i < 10; i++) {
+        bar[6+i] = GLYPH_BAR_OFFSET;
+    }
+    // bar ending decoration
+    memcpy(&bar[6+10], "\x0B\x00", 2);
+
+    // percentage
+    strncat(bar, percent, 4);
+
+    oled_write_ln(bar, false);
+    return;
+}
+
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  memset((void*)&measures, 0, sizeof(measures));
   if (is_keyboard_master())
     return OLED_ROTATION_0;
   else {
@@ -180,21 +275,45 @@ bool oled_task_user(void) {
         oled_write_ln("BASE ", layer_state == L_BASE );
         oled_write_ln("LOWER", layer_state == L_LOWER);
         oled_write_ln(read_mod_state(),true);
+        oled_write_ln("", false);
         oled_write_ln("CAPS", host_keyboard_led_state().caps_lock);
     } else {
-        char name[4+1];
-        char percent[4+1];
+        char name_str_buf[4+1];
+        char percent_str_buf[4+1];
         if (measures.nb_measures == 0) {
             if (prev_was_measures) oled_clear();
             oled_write(read_logo(), false);
             prev_was_measures = false;
         } else {
-            if (!prev_was_measures) oled_clear();
+            if (!prev_was_measures) {
+                oled_clear();
+            }
             for (int i = 0; i < measures.nb_measures; i++) {
-                snprintf(name, sizeof(name), "%.4s", measures.measures[i].name);
-                snprintf(percent, sizeof(percent), "%3d%%", (int) measures.measures[i].percent);
-                oled_write(name, false);
-                oled_write_ln(percent, false);
+                measure_t measure = measures.measures[i]; // avoid glitches due to its volatile nature
+                animation_t anim  = measures.animations[i];
+                int val = (int)(measure.percent);
+                snprintf(name_str_buf,    sizeof(name_str_buf),    "%.4s",  measure.name);
+                snprintf(percent_str_buf, sizeof(percent_str_buf), "%3d%%", val);
+
+                if (anim.nb_steps > 0) {
+                    // do not directly draw the bar according to the value, use transitions
+                    if (anim.current_step < anim.nb_steps) {
+                        if (timer_elapsed(anim.timer) >= anim.ms_period) {
+                            anim.px += anim.increase;
+                            measures.animations[i].px = anim.px;
+                            measures.animations[i].current_step++;
+                            measures.animations[i].timer = timer_read();
+                        }
+                        draw_bar(name_str_buf, percent_str_buf, anim.px);
+                    } else {
+                        // end of animation, ensure a clean ending
+                        draw_bar(name_str_buf, percent_str_buf, PERCENT_TO_PX(val));
+                        measures.animations[i].nb_steps = 0; // disable this animation
+                    }
+                } else {
+                    // no on-going transition, just display the value
+                    draw_bar(name_str_buf, percent_str_buf, PERCENT_TO_PX(val));
+                }
             }
             prev_was_measures = true;
         }
@@ -217,13 +336,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 // stop rendering metrics when data is not received anymore
 uint32_t metrics_timeout(uint32_t trigger_time, void *cb_arg) {
-    bool metrics_rcvd = *((bool*)cb_arg);
-    if (!metrics_rcvd) {
-        // timeout! let's display the logo again
-        measures.nb_measures = 0;
-    }
-    // ms delay, 0 if not to be called again
-    return metrics_rcvd ? GOMETRICS_TIMEOUT_MS : 0;
+    // timeout!
+    measures.nb_measures = 0;
+    // could be useful but horrible, commented out
+    // not to be called again, hence the hackish invalidation of the deferred token
+    /*
+    deferred_token *p_token = (deferred_token*)cb_arg;
+    *p_token = INVALID_DEFERRED_TOKEN;
+    */
+    return 0;
 }
 
 bool raw_hid_receive_user(uint8_t *data, uint8_t length) {
@@ -235,31 +356,35 @@ bool raw_hid_receive_user(uint8_t *data, uint8_t length) {
             // skip the header and read the number of items
             data += sizeof(MAGIC);
             size_t nb_items = (size_t)*data;
-            nb_items = (nb_items > 4) ? 4 : nb_items;
+            nb_items = (nb_items > MAX_NB_MEASURES) ? MAX_NB_MEASURES : nb_items;
 
             measures.nb_measures = nb_items;
             data++;
 
             if (nb_items > 0) {
-                memset((void*)&measures, 0, sizeof(measures)); // all names are null
-                for (int i = 0; i < nb_items; i++, data+=5) {
-                    strncpy((char *)measures.measures[i].name, (const char*)data, 4);
-                    measures.measures[i].percent = (uint8_t)data[4];
-                }
-                metrics_received = true;
-                if (metrics_timeout_token != INVALID_DEFERRED_TOKEN) {
-                    extend_deferred_exec(metrics_timeout_token, GOMETRICS_TIMEOUT_MS);
-                } else {
-                    metrics_timeout_token = defer_exec(GOMETRICS_TIMEOUT_MS, metrics_timeout, (void*)&metrics_received);
-                }
-            } else {
-                metrics_received = false;
-            }
+                for (int i = 0; i < nb_items; i++, data += HIDRAW_DATA_LEN) {
+                    uint8_t cur_value = measures.measures[i].percent;
+                    uint8_t new_value = (uint8_t)data[HIDRAW_NAME_LEN];
 
+                    strncpy((char *)measures.measures[i].name, (const char*)data, HIDRAW_NAME_LEN);
+                    #ifdef OLED_ENABLE
+                    init_animation(i, cur_value, new_value);
+                    #endif
+                    measures.measures[i].percent = new_value;
+                }
+                // timeout, to detect if metrics are no longer updated
+                if (metrics_timeout_token != INVALID_DEFERRED_TOKEN) {
+                    if (!extend_deferred_exec(metrics_timeout_token, GOMETRICS_TIMEOUT_MS)) {
+                        // the timeout was triggered, the callback is not registered anymore
+                        metrics_timeout_token = defer_exec(GOMETRICS_TIMEOUT_MS, metrics_timeout, NULL);
+                    }
+                } else {
+                    metrics_timeout_token = defer_exec(GOMETRICS_TIMEOUT_MS, metrics_timeout, NULL);
+                }
+            }
             return false;
         } else {
             // something else, eg VIA
-            metrics_received = false;
             return true;
         }
     }
@@ -268,3 +393,8 @@ bool raw_hid_receive_user(uint8_t *data, uint8_t length) {
     }
 }
 
+/*
+bool shutdown_user(bool jump_to_bootloader) {
+    return true; // process keyboard level
+}
+*/
